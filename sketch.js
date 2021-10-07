301d195c368363ba76f29fb924afe59c95cfb63a
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-10-07 17:27:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var day;
+
+function setup() {
+  createCanvas(400, 400);
+  
+  //assign random round off number to day
+  
+}
+
+function draw() {
+  background(220);
+
+  textSize(20);
+
+  //add switch case for day name
+  
+}
