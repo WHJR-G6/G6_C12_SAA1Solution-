9ad0f4a7b82ec52267287e5d2da9182c92d3396f
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-10-07 17:29:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ function setup() {
   createCanvas(400, 400);
   
   //assign random round off number to day
+  day = Math.round(1,7)
   
 }
 
@@ -13,5 +14,22 @@ function draw() {
   textSize(20);
 
   //add switch case for day name
+  switch(day) {
+	case 1 : 
+    	text("Sunday", 150,200);
+        break;
+      text("Monday", 150,200);
+        break;
+      text("Tuesday", 150,200);
+        break;
+      text("Wednesday", 150,200);
+        break;
+      text("Thursday", 150,200);
+        break;
+      text("Friday", 150,200);
+        break;
+       text("Saturday", 150,200);
+        break;
+}
   
 }
